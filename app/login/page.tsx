72afbf6022d566ae7fecdcb24d4062f9485c750a
P541M <videna.psalmeleazar@gmail.com>
2025-02-22 06:48:12 -0500

    fixed ui

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,8 +2,7 @@ import AuthForm from "../components/AuthForm";
 
 export default function LoginPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold mb-6">Login to Frij.io</h1>
+    <div>
       <AuthForm />
     </div>
   );
